64706e26bf13751827fa093aa141aabb5d8ffd8f
MinhyeokSun <s.minyeok@gmail.com>
2021-12-19 14:05:44 +0900

    3rd Commit

--- app/src/main/java/com/smh/pomodoro_timer/MainActivity.kt
+++ app/src/main/java/com/smh/pomodoro_timer/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.smh.pomodoro_timer
 
 import android.annotation.SuppressLint
+import android.media.SoundPool
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.util.Log
 import android.widget.SeekBar
 import android.widget.TextView
 
@@ -21,12 +23,19 @@ class MainActivity : AppCompatActivity() {
         findViewById<SeekBar>(R.id.seekBar)
     }
 
+    private val soundPool = SoundPool.Builder().build()
+
+    private var tickingSoundId: Int? = null
+
+    private var bellSoundId: Int? = null
+
     private var currentCountDownTimer: CountDownTimer? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         bindViews()
+        initSounds()
     }
 
     private fun bindViews() {
@@ -47,10 +56,19 @@ class MainActivity : AppCompatActivity() {
                     seekBar ?: return
                     currentCountDownTimer = createCountDownTimer(seekBar.progress * 60 * 1000L)
                     currentCountDownTimer?.start()
+
+                    tickingSoundId?.let { soundId ->
+                        soundPool.play(soundId, 1F, 1F, 0, -1, 1F)
+                    }
                 }
             }
 
         )
+    }
+
+    private fun initSounds() {
+        tickingSoundId = soundPool.load(this, R.raw.timer_ticking, 1)
+        bellSoundId = soundPool.load(this, R.raw.timer_bell, 1)
     }
 
     private fun createCountDownTimer(initialMillis: Long): CountDownTimer {
